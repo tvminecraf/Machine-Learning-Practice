8c493af7a533512c760ad2cd031b8a7567aa1fe7
tvminecraf <m-emrekalaycik@hotmail.com>
2024-03-20 07:45:27 +0300

    all files added to git repo folder

---
+++ Stars/Custom.py
@@ -0,0 +1,75 @@
+import numpy as np
+from sklearn.metrics import (
+    classification_report,
+    f1_score,
+    mean_absolute_error,
+    mean_squared_error,
+    precision_score,
+    recall_score,
+    confusion_matrix,
+)
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsClassifier
+from matplotlib import pyplot
+from seaborn import heatmap
+
+
+def Matrix(test:list, predict:list):
+    pyplot.figure(figsize=(15, 8))
+    heatmap(confusion_matrix(test, predict), annot=True)
+    pyplot.ylabel("Prediction", fontsize=13)
+    pyplot.xlabel("Actual", fontsize=13)
+    pyplot.title("Confusion Matrix", fontsize=17)
+    pyplot.show()
+
+
+def Summary(Model_Name:str, test:list, predict:list):
+    print(
+        f"Classification Report for {Model_Name}:\n",
+        classification_report(test, predict),
+        "\n",
+        f"F1 Score for {Model_Name}:",
+        f1_score(test, predict, average="weighted"),
+        "\n",
+        f"Precision Score for {Model_Name}:",
+        precision_score(test, predict, average="weighted"),
+        "\n",
+        f"Recall Score for {Model_Name}:",
+        recall_score(test, predict, average="weighted"),
+        "\n",
+        f"Confusion Matrix for {Model_Name}:",
+        confusion_matrix(test, predict),
+    )
+
+
+def All(Model_Name: str, test: list, predict: list):
+    Summary(Model_Name, test, predict)
+    Matrix(test, predict)
+
+def Knn_Greedy(X_train: list,X_test: list,y_train: list,y_test: list,iter: bool = False,return_mode: int = 0,):
+    """
+    Bu fonksiyon, KNN algoritmasının en iyi k değerini bulmak için kendi yazdığım bir arama algoritmasıdır.
+    Eğer veri setiniz çok büyük ve sistemiz güçlü değilse, bu fonksiyonu kullanmanızı önermem.
+    Fonsiyon girdi olarak X_train, X_test, y_train, y_test alır ve en iyi k değerini ve skoru döndürür.
+    Eğerki arkada kaçıncı döngüde olduğunu görmek istiyorsanız, iter değişkenini True yapabilirsiniz.
+    Retun mode 0 ise sadece skoru döndürür, 1 ise sadece k değerini döndürür, 2 ise hem skoru hem k değerini döndürür.
+    """
+    score = 0
+    for i in range(1, len(X_train), 2):
+        knn = KNeighborsClassifier(n_neighbors=i)
+        knn.fit(X_train, y_train)
+        _temp = knn.score(X_test, y_test)
+        if _temp > score:
+            score = _temp
+            KNeighborsClassifier_Max_neighbors = i
+        if iter:
+            print(f"Score: {_temp} | K: {i}")
+    if return_mode == 0:
+        return score
+    elif return_mode == 1:
+        return KNeighborsClassifier_Max_neighbors
+    elif return_mode == 2:
+        return score, KNeighborsClassifier_Max_neighbors
+    else:
+        return "Error: return_mode must be 0, 1 or 2"
+
---
+++ custom.py
@@ -0,0 +1,88 @@
+def compare():
+    pass
+
+def tester(model,data):
+
+    pass
+
+
+
+
+
+"""
+x modeli :
+    a bırakıldı doğruluk: x
+    b bırakıldı doğruluk: y 
+    a,b bırakıldı doğruluk: z
+y modeli 
+    a bırakıldı doğruluk: x
+    b bırakıldı doğruluk: y 
+    a,b bırakıldı doğruluk: z
+    
+best result z model a,b bırakıldı doğruluk z
+"""
+
+"""
+Modeller:
+    K-Means Clustering,  
+    K-Nearest Neighbor
+    Decision Tree Learning	
+    ID3 and C4.5 algorithms
+    Regression Trees
+    Naive Bayes Classifier,
+    Linear Regression
+    Classification Trees 
+    Regression Trees
+    Support Vector Machines (SVM)
+    Random Forest
+    Logistic Regression
+    Neural Networks
+    Gradient Boosting Machines
+    AdaBoost
+    Bagging
+    Stochastic Gradient Descent
+    Principal Component Analysis (PCA)
+    Linear Discriminant Analysis (LDA)
+    Quadratic Discriminant Analysis (QDA)
+    K-Nearest Neighbors (KNN)
+    Hierarchical Clustering
+    DBSCAN
+
+
+    """
+
+"""
+    Modeller(No Ai)
+knn
+svm
+naive bayes
+random forest
+logistic regression
+decision tree
+linear regression
+ID3 and C4.5 algorithms
+"""
+"""
+Methodlar:
+    Entropy
+    Least Squares Method
+    Gini Index
+    Information Gain
+    Chi-Square
+
+    Artificial Neural Networks (ANN)
+    Principal Component Analysis (PCA)
+    Gradient Boosting
+    AdaBoost
+    K-Means Clustering
+    Hierarchical Clustering
+
+"""
+"""
+nereye bilemedim
+    Thresholding and Competitive Classification
+    Fuzzy Logic, Fuzzy Inference Systems
+    
+
+        
+"""
